9c31b3c3678db50b8dd964a7373d1c2600dd647b
ziumdev <81539675+ziumdev@users.noreply.github.com>
2021-05-06 12:47:44 +0900

    Create TrapReceiverServer.py
    snmpV2

---
+++ TrapReceiverServer.py
@@ -0,0 +1,47 @@
+from pysnmp.entity import engine, config
+from pysnmp.carrier.asyncore.dgram import udp
+from pysnmp.entity.rfc3413 import ntfrcv
+import logging
+
+snmpEngine = engine.SnmpEngine()
+
+TrapAgentAddress='127.0.0.1' #Trap listerner address
+Port=50003  #trap listerner port
+
+logging.basicConfig(filename='received_traps.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
+
+logging.info("Agent is listening SNMP Trap on "+TrapAgentAddress+" , Port : " +str(Port))
+logging.info('--------------------------------------------------------------------------')
+
+print("Agent is listening SNMP Trap on "+TrapAgentAddress+" , Port : " +str(Port));
+
+config.addTransport(
+    snmpEngine,
+    udp.domainName + (1,),
+    udp.UdpTransport().openServerMode((TrapAgentAddress, Port))
+)
+
+#Configure community here
+config.addV1System(snmpEngine, 'my-area', 'test')
+
+
+def cbFun(snmpEngine, stateReference, contextEngineId, contextName, varBinds, cbCtx):
+    print("Received new Trap message");
+    logging.info("Received new Trap message")
+    for name, val in varBinds:
+        logging.info('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+        print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+
+    logging.info("==== End of Incoming Trap ====")
+
+
+ntfrcv.NotificationReceiver(snmpEngine, cbFun)
+
+snmpEngine.transportDispatcher.jobStarted(1)
+
+try:
+    snmpEngine.transportDispatcher.runDispatcher()
+except Exception as e:
+    snmpEngine.transportDispatcher.closeDispatcher()
+    print(e)
+    raise
