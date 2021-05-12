76368b87bdb505786407f93ab0a4eea68b556c1e
ziumdev <81539675+ziumdev@users.noreply.github.com>
2021-05-12 10:36:37 +0900

    Update snmpToMqttTest.py
    메인 라이브러리를 pysnmp로 변경함

--- snmpToMqttTest.py
+++ snmpToMqttTest.py
@@ -2,42 +2,39 @@
 # /etc/snmp/snmpd 에 sysContact, sysLocation, sysServices 정보가 들어가 있음
 # /etc/snmp/snmpd 의 Manager에 들어가는 커뮤니티명은 rocommunity에서 정보를 바꿈
 
-import time
-from snmp import Manager
-from snmp.exceptions import Timeout
+from pysnmp.hlapi import *
 import mqttConfig
 import json
+host = '127.0.0.1'
+port = 161
 
-# REPLACE 'public' with your community string
-manager = Manager(b'test')
+oidList = ["1.3.6.1.2.1.1.4.0", "1.3.6.1.2.1.1.5.0", "1.3.6.1.2.1.1.6.0", "1.3.6.1.2.1.1.7.0"]
+mqttTopic = 'snmp/test'
 
-try:
-    hosts = ["127.0.0.1"]                    # REPLACE these IPs with real IPs      localhost로 입력하면 안됨
-    oids = ["1.3.6.1.2.1.1.4.0", "1.3.6.1.2.1.1.5.0", "1.3.6.1.2.1.1.6.0", "1.3.6.1.2.1.1.7.0"]   # [SNMPv2-MIB::sysDescr.0, SNMPv2-MIB::sysName.0]
 
-    start = time.time()
+def getData(oids):
+    data = {}
+    for oid in oids:
+        iterator = getCmd(SnmpEngine(), CommunityData('test'), UdpTransportTarget((host, port)), ContextData(),
+                      ObjectType(ObjectIdentity(oid)))
+        errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+        if errorIndication:  # SNMP engine errors
+            print(errorIndication)
+        else:
+            if errorStatus:  # SNMP agent errors
+                print('%s at %s' % (errorStatus.prettyPrint(), varBinds[int(errorIndex)-1] if errorIndex else '?'))
+            else:
+                for varBind in varBinds:  # SNMP response contents
+                    data[str(varBind).split('=')[0]] = str(varBind).split('=')[1]
+                    print(varBind)
+    return data
 
-    # removing this loop will increase run time on average
-    for host in hosts:
-        manager.get(host, *oids, block=False, timeout=1)
-        manager.get(host, *oids, block=False, timeout=1, next=True)
 
-    for host in hosts:
-        vars = manager.get(host, *oids)
-        print(host)
-        for var in vars:
-            print(var)
-            mqttConfig.mqClient.publish(topic='snmp/test', payload=str(var), qos=0) # 차후 json 형태로 바꿔야함
+def sendData(topic, payload, qos):
+    mqttConfig.mqClient.publish(topic=topic, payload=json.dumps(payload), qos=qos)
+    print("success")
 
-        # vars = manager.get(host, *oids, next=True)
-        # for var in vars:
-        #     print(var)
+if __name__ == '__main__':
+    mqttPayload = getData(oidList)
+    sendData(mqttTopic, mqttPayload, 0)
 
-    end = time.time()
-    print("Took {} seconds".format(end - start))
-
-except Timeout as e:
-    print("Request for {} from host {} timed out".format(e, host))
-
-finally:
-    manager.close()
